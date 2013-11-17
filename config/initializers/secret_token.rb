1e6d8b72fd254551fb7956a213f5b3bf788c48dd
arnaud sellenet <arnodmental@gmail.com>
2013-11-17 22:58:46 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Embertodo::Application.config.secret_key_base = 'e5d51666fd6b4eab5c9622e0601ad60b3730d98c02d86e0243b087f157419fa9b24073a1e1e6c2ae3b3ee176e9510fd8fd643c0ea434a0768ef7c3e1e4a0a84d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Embertodo::Application.config.session_store :cookie_store, key: '_embertodo_session'
